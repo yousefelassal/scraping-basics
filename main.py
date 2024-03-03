b3d478e5a5572220bb1b8bf735403c03e00453de
Yousef <yousefsameh24@gmail.com>
2024-03-03 21:45:21 +0200

    create main

---
+++ main.py
@@ -0,0 +1,8 @@
+import requests
+from bs4 import BeautifulSoup
+
+webpage_response = requests.get('https://content.codecademy.com/courses/beautifulsoup/shellter.html')
+
+webpage = webpage_response.content
+soup = BeautifulSoup(webpage, "html.parser")
+print(soup)
